0c179c2e21435737da9dec91b21ad9ca8e80fcce
s-a-tselikov <satselikov@gmail.com>
2026-05-31 18:06:54 +0400

    Remove Tilda CDN and Ticketscloud scripts from static export.
    Stops loading external Tilda/tickets widgets that triggered browser warnings on /eng.

--- prepare-pages.js
+++ prepare-pages.js
@@ -2,6 +2,29 @@ const fs = require('fs');
 const path = require('path');
 
 const root = __dirname;
+
+function sanitizeExportHtml(html) {
+  return html
+    .replace(/ <link rel="dns-prefetch" href="https:\/\/ws\.tildacdn\.com">/g, '')
+    .replace(
+      / <script src="https:\/\/neo\.tildacdn\.com\/js\/tilda-fallback-1\.0\.min\.js" async charset="utf-8"><\/script>/g,
+      ''
+    )
+    .replace(
+      /<div id="rec\d+" class="r t-rec" style=" " data-animationappear="off" data-record-type="131"> <!-- T123 --> <div class="t123"> <div class="t-container_100 "> <div class="t-width t-width_100 "> <!-- nominify begin --> <script src="https:\/\/ticketscloud\.com\/static\/scripts\/widget\/tcwidget\.js"><\/script> <!-- nominify end --> <\/div> <\/div> <\/div> <\/div> /g,
+      ''
+    )
+    .replace(/href="#ticketscloud:[^"]+"/g, 'href="#tickets"')
+    .replace(/data-tilda-lazy="yes"/g, 'data-tilda-lazy="no"');
+}
+
+const sources = ['index.html', 'english.html', 'georgian.html', '404.html'];
+for (const file of sources) {
+  const filePath = path.join(root, file);
+  const sanitized = sanitizeExportHtml(fs.readFileSync(filePath, 'utf8'));
+  fs.writeFileSync(filePath, sanitized);
+}
+
 const routes = [
   ['index.html', 'ru/index.html'],
   ['english.html', 'eng/index.html'],
